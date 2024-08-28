a3c27630f1032764770be740d4044ff836aac362
CLINTON <clintonchiemeka@gmail.com>
2024-08-28 13:28:54 +0100

    updated task 6

--- app.js
+++ app.js
@@ -16,21 +16,3 @@ app.use(internalServerError);
 
 module.exports = app;
 
-/*
-CORE: GET /api/articles/:article_id/comments
-Description
-Should:
-be available on /api/articles/:article_id/comments.
-get all comments for an article.
-Responds with:
-an array of comments for the given article_id of which each comment should have the following properties:
-comment_id
-votes
-created_at
-author
-body
-article_id
-Comments should be served with the most recent comments first.
-Consider what errors could occur with this endpoint, and make sure to test for them.
-Remember to add a description of this endpoint to your /api endpoint.
-*/

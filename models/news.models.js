ecc76bf6c826f3fccf806f0c31cafc94e8aac590
CLINTON <clintonchiemeka@gmail.com>
2024-09-01 22:08:27 +0100

    Returns a user object by username

--- models/news.models.js
+++ models/news.models.js
@@ -84,7 +84,7 @@ exports.selectedArticlesComments = async (article_id) => {
   return rows;
 };
 
-exports.insertComment = async (article_id, username, body) => {
+exports.insertComment = (article_id, username, body) => {
   return db
     .query("SELECT * FROM articles WHERE article_id = $1", [article_id])
     .then(({ rows }) => {
@@ -109,34 +109,39 @@ exports.insertComment = async (article_id, username, body) => {
     });
 };
 
-exports.insertVote = async (article_id, votes) => {
-  return db
+exports.insertVote =  async (article_id, votes) => {
+  const { rows } = await db
     .query(
       "UPDATE articles SET votes = votes + $1 WHERE article_id = $2 RETURNING *",
       [votes, article_id]
-    )
-    .then(({ rows }) => {
-      if (rows.length === 0) {
-        return Promise.reject({ status: 404, msg: "Invalid article ID" });
-      }
-      return rows[0];
-    });
+    );
+  if (rows.length === 0) {
+    return Promise.reject({ status: 404, msg: "Invalid article ID" });
+  }
+  return rows[0];
 };
 
 exports.deletedComment = async (comment_id) => {
-  return db
+  const { rows } = await db
     .query(`DELETE FROM comments WHERE comment_id = $1 RETURNING *;`, [
       comment_id,
-    ])
-    .then(({ rows }) => {
-      if (rows.length === 0) {
-        return Promise.reject({ status: 404, msg: "Comment not found" });
-      }
-      return rows[0];
-    });
+    ]);
+  if (rows.length === 0) {
+    return Promise.reject({ status: 404, msg: "Comment not found" });
+  }
+  return rows[0];
 };
 
 exports.selectedUsers = async () => {
   const { rows } = await db.query(`SELECT * FROM users`);
   return rows;
 };
+
+exports.selectedUser = async ( username ) => {
+  const { rows } = await db
+    .query(`SELECT * FROM users WHERE username = $1`, [username]);
+  if (rows.length === 0) {
+    return Promise.reject({ status: 404, msg: "Invalid username" });
+  }
+  return rows[0];
+}
